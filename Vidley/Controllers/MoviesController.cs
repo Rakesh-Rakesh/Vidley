f36526edd1586fb81fe09b025d06ff5d160e127a
rakeshjak225@gmail.com <rakeshjak225@gmail.com>
2021-12-23 22:20:51 +0530

    InitialStage

---
+++ Vidley/Controllers/MoviesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidley.Models;
+using Vidley.ViewModels;
+
+namespace Vidley.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Shrek !"
+            };
+            var customers = new List<Customer>
+            {
+                new Customer{Name="CUstomer 1"},
+                new Customer{Name="CUstomer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel() {
+                Movie = movie,
+                Customers=customers
+            };
+
+
+            return View(viewModel);
+        }
+        
+        public ActionResult ByReleaseDate(int year,int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
