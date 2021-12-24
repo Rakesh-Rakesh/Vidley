d3435ab3b040d313272e0735a0b87e24bbdc70cc
rakeshjak225@gmail.com <rakeshjak225@gmail.com>
2021-12-24 21:15:01 +0530

    MovieModelandMembershipAdding

---
+++ Vidley/Controllers/CustomersController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidley.Models;
+using System.Data.Entity;
+
+namespace Vidley.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ActionResult New()
+        {
+            //var membershipTypes=
+            return View();
+        }
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int Id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == Id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+    }
+}
